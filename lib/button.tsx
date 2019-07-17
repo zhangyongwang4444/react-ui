ce58f0ed50f95d3375c014f4118ad3665043ab93
zhangyongwang <20225614@qq.com>
2019-07-17 16:32:26 +0800

    测试配置好了！

--- lib/button.tsx
+++ lib/button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 function Button() {
     return (
         <div>
-            button2
+            <div>button2</div>
         </div>
     )
 }
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,6 +2,6 @@
 import Button from './button';
 
 const a = 1
-
+console.log('hi')
 export { Button, a }
 
