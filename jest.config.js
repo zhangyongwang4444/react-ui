daf0b45d6ca3558f075ac7ec6bc72b7dc104e0c4
zhangyongwang <20225614@qq.com>
2019-07-22 20:24:15 +0800

    继续配置 单元测试 环境

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     verbose: true,
     clearMocks: false,
     collectCoverage: true,
-    reporters: ["default"],
+    reporters: ["default", "jest-junit"],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules'],
     globals: {
