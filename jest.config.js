fd9efe6c53bff7c0887a2f6f3907b206e24106cb
zhangyongwang <20225614@qq.com>
2019-07-22 20:49:02 +0800

    配置 jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,13 @@
 module.exports = {
     verbose: true,
     clearMocks: false,
+    reporters: ["default"],
+
     collectCoverage: true,
-    reporters: ["default", "jest-junit"],
+    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules'],
     globals: {
