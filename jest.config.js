5b6b050f1b6b29b67520fa295f369223f107df82
zhangyongwang <20225614@qq.com>
2019-07-23 11:17:11 +0800

    更新 jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     verbose: true,
     clearMocks: false,
-    reporters: ["default","jest-junit"],
+    reporters: ["default"],
 
     collectCoverage: true,
     collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
