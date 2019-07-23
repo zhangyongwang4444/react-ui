befe282a6a11de3784db785873831d13b825bc7c
zhangyongwang <20225614@qq.com>
2019-07-23 14:50:58 +0800

    导出 Icon 组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,19 +1,4 @@
-import ReactDOM from "react-dom";
-import * as React from "react";
-import Icon from './icon/icon';
+export {default as Icon} from './icon/icon';
 
 
-const fn:React.MouseEventHandler<SVGElement> = (e)=>{
-    console.log(e.target);
-};
-
-ReactDOM.render(<div>
-    <Icon name="wechat"
-          className="qqqqq"
-          onClick={fn}
-          onMouseEnter={()=>console.log('enter')}
-          onMouseLeave={()=>console.log('leave')}
-          onTouchStart={()=>console.log('touchstart')}
-    />
-</div>, document.querySelector('#root'));
 
