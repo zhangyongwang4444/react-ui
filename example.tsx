358f5f782eb82c0c6ba2e786514ffba6bc480773
zhangyongwang <20225614@qq.com>
2019-07-24 11:00:08 +0800

    写 测试页面

---
+++ example.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render((
+    <div>
+        <header>
+            <div className="logo">
+                react-ui
+            </div>
+        </header>
+        <div>
+            <aside>
+                <h2>组件</h2>
+                <ul>
+                    <li>Icon</li>
+                    <li>Button</li>
+                </ul>
+            </aside>
+            <main>
+
+            </main>
+        </div>
+    </div>
+), document.querySelector('#root'));
+
