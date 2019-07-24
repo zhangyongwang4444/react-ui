fe3d6237a1fce1cf853d4d17b9fa85cb7e779718
zhangyongwang <20225614@qq.com>
2019-07-24 12:01:02 +0800

    add examples

--- example.tsx
+++ example.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import IconExample from "./lib/icon/icon.example";
+import ButtonExample from "./lib/button.example";
 
-
-ReactDOM.render((
-    <div>
-        <header>
-            <div className="logo">
-                react-ui
-            </div>
-        </header>
+ReactDOM.render(
+    <Router>
         <div>
-            <aside>
-                <h2>组件</h2>
-                <ul>
-                    <li>Icon</li>
-                    <li>Button</li>
-                </ul>
-            </aside>
-            <main>
-
-            </main>
+            <header>
+                <div className="logo">
+                    react-ui
+                </div>
+            </header>
+            <div>
+                <aside>
+                    <h2>组件</h2>
+                    <ul>
+                        <li><Link to="/icon">Icon</Link></li>
+                        <li><Link to="/button">Button</Link></li>
+                    </ul>
+                </aside>
+                <main>
+                    <Route path="/icon" component={IconExample}/>
+                    <Route path="/button" component={ButtonExample}/>
+                </main>
+            </div>
         </div>
-    </div>
-), document.querySelector('#root'));
+    </Router>
+    , document.querySelector('#root'));
 
---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Icon from "./icon";
+
+const IconExample: React.FunctionComponent = () => {
+    return (
+        <div>
+            <Icon name="wechat"/>
+            <Icon name="alipay"/>
+            <Icon name="google"/>
+            <Icon name="qq"/>
+        </div>
+    )
+};
+
+export default IconExample;
