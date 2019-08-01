bd3519b5094a7d397975146989cdf1a0432fcaf7
zhangyongwang <20225614@qq.com>
2019-08-01 17:29:48 +0800

    写一个 scopedClassMaker 函数

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -2,23 +2,27 @@ import * as React from "react";
 import './dialog.scss';
 import {Fragment} from "react";
 import {Icon} from "../index";
+import {scopedClassMaker} from "../classes";
 
 interface Props {
     visible:boolean;
 }
 
+const scopedClass = scopedClassMaker('react-ui-dialog');
+const sc= scopedClass;
+
 const  Dialog:React.FunctionComponent<Props> =(props)=> {
     return(
         props.visible ?
             <Fragment>
-                <div className="react-ui-dialog-mask"/>
-                <div className="react-ui-dialog">
-                    <div className="react-ui-dialog-close">
+                <div className={sc('mask')}/>
+                <div className={sc()}>
+                    <div className={sc('close')}>
                         <Icon name="close"/>
                     </div>
-                    <header className="react-ui-dialog-header">提示</header>
-                    <main className="react-ui-dialog-main"> {props.children}</main>
-                    <footer className="react-ui-dialog-footer">
+                    <header className={sc('header')}>提示</header>
+                    <main className={sc('main')}> {props.children}</main>
+                    <footer className={sc('footer')}>
                         <button>ok</button>
                         <button>cancel</button>
                     </footer>
