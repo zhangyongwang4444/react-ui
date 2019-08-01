880a4820b4096ba63500a92e4c303a0a91b9ea57
zhangyongwang <20225614@qq.com>
2019-08-01 16:54:50 +0800

    完成 Dialog 的基本样式

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import './dialog.scss';
 import {Fragment} from "react";
+import {Icon} from "../index";
 
 interface Props {
     visible:boolean;
@@ -10,9 +11,17 @@ const  Dialog:React.FunctionComponent<Props> =(props)=> {
     return(
         props.visible ?
             <Fragment>
-                <div className="react-ui-dialog-mask"></div>
+                <div className="react-ui-dialog-mask"/>
                 <div className="react-ui-dialog">
-                    {props.children}
+                    <div className="react-ui-dialog-close">
+                        <Icon name="close"/>
+                    </div>
+                    <header className="react-ui-dialog-header">提示</header>
+                    <main className="react-ui-dialog-main"> {props.children}</main>
+                    <footer className="react-ui-dialog-footer">
+                        <button>ok</button>
+                        <button>cancel</button>
+                    </footer>
                 </div>
             </Fragment> :
             null
