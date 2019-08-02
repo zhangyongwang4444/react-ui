6ff90c8d4eff3fa4736748b538ae285cdfebb70b
zhangyongwang <20225614@qq.com>
2019-08-02 15:48:52 +0800

    React.cloneElement 的用法

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -2,11 +2,22 @@ import React, {useState} from 'react'
 import Dialog from "./dialog";
 
 export default function () {
-    const [x,setX] = useState(false);
+    const [x, setX] = useState(false);
     return (
         <div>
-            <button onClick={()=>setX(!x)}>click</button>
-            <Dialog visible={x}>
+            <button onClick={() => setX(!x)}>click</button>
+            <Dialog visible={x} buttons={
+                [
+                    <button onClick={() => {
+                        setX(false)
+                    }}>1</button>,
+                    <button onClick={() => {
+                        setX(false)
+                    }}>2</button>
+                ]
+            } onClose={() => {
+                setX(false)
+            }}>
                 <div>hi</div>
             </Dialog>
         </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,30 +1,36 @@
 import * as React from "react";
 import './dialog.scss';
-import {Fragment} from "react";
+import {Fragment, ReactElement} from "react";
 import {Icon} from "../index";
 import {scopedClassMaker} from "../classes";
 
 interface Props {
-    visible:boolean;
+    visible: boolean;
+    buttons: Array<ReactElement>
+    onClose: React.MouseEventHandler
 }
 
 const scopedClass = scopedClassMaker('react-ui-dialog');
-const sc= scopedClass;
+const sc = scopedClass;
 
-const  Dialog:React.FunctionComponent<Props> =(props)=> {
-    return(
+const Dialog: React.FunctionComponent<Props> = (props) => {
+    const onClickClose: React.MouseEventHandler = (e) => {
+        props.onClose(e);
+    };
+    return (
         props.visible ?
             <Fragment>
                 <div className={sc('mask')}/>
                 <div className={sc()}>
-                    <div className={sc('close')}>
+                    <div className={sc('close')} onClick={onClickClose}>
                         <Icon name="close"/>
                     </div>
                     <header className={sc('header')}>提示</header>
                     <main className={sc('main')}> {props.children}</main>
                     <footer className={sc('footer')}>
-                        <button>ok</button>
-                        <button>cancel</button>
+                        {props.buttons.map((button, index) => {
+                            React.cloneElement(button, {key: index})
+                        })}
                     </footer>
                 </div>
             </Fragment> :
