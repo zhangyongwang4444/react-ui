75c8ce6f31978e1399768fa89841f88d420c546c
zhangyongwang <20225614@qq.com>
2019-08-03 20:34:12 +0800

    Layout 组件的准备工作

---
+++ lib/layout/layout.example.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from "./layout";
+import Header from "./header";
+import Content from "./content";
+import Footer from "./footer";
+
+export default function () {
+    return (
+        <div>
+            <div>
+                <h1>第一个例子</h1>
+                <Layout>
+                    <Header>header</Header>
+                    <Content>content</Content>
+                    <Footer>footer</Footer>
+                </Layout>
+            </div>
+        </div>
+    );
+};
