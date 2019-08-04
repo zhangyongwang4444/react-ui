10e3f79a408f6895659f1bec50be235447bbf9db
zhangyongwang <20225614@qq.com>
2019-08-04 10:43:47 +0800

    又写了 layout 的几个示例

--- lib/layout/aside.tsx
+++ lib/layout/aside.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {scopedClassMaker} from "../classes";
 
-const Aside: React.FunctionComponent = () => {
+const sc = scopedClassMaker('react-ui-layout');
+
+interface Props extends React.HTMLAttributes<HTMLElement> {
+
+}
+
+const Aside: React.FunctionComponent<Props> = (props) => {
+    const {className, ...rest} = props;
     return (
-        <div>aside</div>
+        <div className={sc('aside', {extra: className})} {...rest} >
+            aside
+        </div>
     )
 };
 
 export default Aside;
+
--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -3,6 +3,7 @@ import Layout from "./layout";
 import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
+import Aside from "./aside";
 
 export default function () {
     return (
@@ -13,6 +14,39 @@ export default function () {
                     <Header>header</Header>
                     <Content>content</Content>
                     <Footer>footer</Footer>
+                </Layout>
+            </div>
+            <div>
+                <h1>第二个例子</h1>
+                <Layout style={{height: 500}} className="hi">
+                    <Header>header</Header>
+                    <Layout>
+                        <Aside>aside</Aside>
+                        <Content>content</Content>
+                    </Layout>
+                    <Footer>footer</Footer>
+                </Layout>
+            </div>
+            <div>
+                <h1>第三个例子</h1>
+                <Layout style={{height: 500}} className="hi">
+                    <Header>header</Header>
+                    <Layout>
+                        <Content>content</Content>
+                        <Aside>aside</Aside>
+                    </Layout>
+                    <Footer>footer</Footer>
+                </Layout>
+            </div>
+            <div>
+                <h1>第四个例子</h1>
+                <Layout style={{height: 500}} className="hi">
+                    <Aside>aside</Aside>
+                    <Layout>
+                        <Header>header</Header>
+                        <Content>content</Content>
+                        <Footer>footer</Footer>
+                    </Layout>
                 </Layout>
             </div>
         </div>
