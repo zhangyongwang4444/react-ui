d03f0f907dbee3452c517bd35346ab8c25374f20
zhangyongwang <20225614@qq.com>
2019-08-04 17:18:59 +0800

    检测 children 的 type , 修改 layout.scss

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -8,36 +8,36 @@ import Aside from "./aside";
 export default function () {
     return (
         <div>
-            <div>
-                <h1>第一个例子</h1>
-                <Layout style={{height: 500}} className="hi">
-                    <Header>header</Header>
-                    <Content>content</Content>
-                    <Footer>footer</Footer>
-                </Layout>
-            </div>
-            <div>
-                <h1>第二个例子</h1>
-                <Layout style={{height: 500}} className="hi">
-                    <Header>header</Header>
-                    <Layout>
-                        <Aside>aside</Aside>
-                        <Content>content</Content>
-                    </Layout>
-                    <Footer>footer</Footer>
-                </Layout>
-            </div>
-            <div>
-                <h1>第三个例子</h1>
-                <Layout style={{height: 500}} className="hi">
-                    <Header>header</Header>
-                    <Layout>
-                        <Content>content</Content>
-                        <Aside>aside</Aside>
-                    </Layout>
-                    <Footer>footer</Footer>
-                </Layout>
-            </div>
+            {/*<div>*/}
+            {/*    <h1>第一个例子</h1>*/}
+            {/*    <Layout style={{height: 500}} className="hi">*/}
+            {/*        <Header>header</Header>*/}
+            {/*        <Content>content</Content>*/}
+            {/*        <Footer>footer</Footer>*/}
+            {/*    </Layout>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <h1>第二个例子</h1>*/}
+            {/*    <Layout style={{height: 500}} className="hi">*/}
+            {/*        <Header>header</Header>*/}
+            {/*        <Layout>*/}
+            {/*            <Aside>aside</Aside>*/}
+            {/*            <Content>content</Content>*/}
+            {/*        </Layout>*/}
+            {/*        <Footer>footer</Footer>*/}
+            {/*    </Layout>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <h1>第三个例子</h1>*/}
+            {/*    <Layout style={{height: 500}} className="hi">*/}
+            {/*        <Header>header</Header>*/}
+            {/*        <Layout>*/}
+            {/*            <Content>content</Content>*/}
+            {/*            <Aside>aside</Aside>*/}
+            {/*        </Layout>*/}
+            {/*        <Footer>footer</Footer>*/}
+            {/*    </Layout>*/}
+            {/*</div>*/}
             <div>
                 <h1>第四个例子</h1>
                 <Layout style={{height: 500}} className="hi">
--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {scopedClassMaker} from "../classes";
 import './layout.scss';
+import Aside from "./aside";
 
 const sc = scopedClassMaker('react-ui-layout');
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
-
+    children: ReactElement | Array<ReactElement>
 }
 
 const Layout: React.FunctionComponent<Props> = (props) => {
     const {className, ...rest} = props;
+    console.log(props.children);
+    let hasAside = false;
+    if ((props.children as Array<ReactElement>).length) {
+        (props.children as Array<ReactElement>).map(node => {
+            if (node.type === Aside) {
+                hasAside = true;
+            }
+        })
+    }
+
     return (
-        <div className={sc('', {extra: className})} {...rest} >
+        <div className={sc('', {extra: [className, hasAside && 'hasAside'].join(' ')})} {...rest} >
             {props.children}
         </div>
     )
