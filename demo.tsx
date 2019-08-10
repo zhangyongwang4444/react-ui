dec8438ec73a8ceca51ed3a27e32534c6037eb9f
zhangyongwang <20225614@qq.com>
2019-08-10 12:20:40 +0800

    代码高亮

--- demo.tsx
+++ demo.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Highlight, {defaultProps} from "prism-react-renderer";
 
 interface Props {
     code: string;
@@ -8,9 +9,19 @@ const Demo: React.FunctionComponent<Props> = (props) => {
     return (
         <div>
             {props.children}
-            <pre>
-                {props.code}
+            <Highlight {...defaultProps} code={props.code} language="jsx">
+                {({className, style, tokens, getLineProps, getTokenProps}) => (
+                    <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                  <div {...getLineProps({line, key: i})}>
+                      {line.map((token, key) => (
+                          <span {...getTokenProps({token, key})} />
+                      ))}
+                  </div>
+              ))}
             </pre>
+                )}
+            </Highlight>
         </div>
     )
 };
