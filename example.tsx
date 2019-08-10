3713d0abd49431b80f374febd05dca56d347043d
zhangyongwang <20225614@qq.com>
2019-08-10 11:19:30 +0800

    封装一个 Demo，用来同时展示 IconExample 和 代码

--- example.tsx
+++ example.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router, Route, NavLink} from 'react-router-dom';
-import IconExample from "./lib/icon/icon.example";
 import ButtonExample from "./lib/button.example";
 import DialogExample from "./lib/dialog/dialog.example";
 import LayoutExample from "./lib/layout/layout.example";
 import {Layout, Header, Aside, Content, Footer} from "./lib/layout/layout";
 import './example.scss';
+import IconDemo from "./lib/icon/icon.demo";
 
-const x = require('!!raw-loader!./lib/icon/icon.example.tsx'); // require 支持自定义加载方式; !! 开启自定义加载方式的标志
-console.log(x.default);
+
 const logo = require('./logo.png'); // 动态加载
 
 ReactDOM.render(
@@ -31,7 +30,7 @@ ReactDOM.render(
                     </ul>
                 </Aside>
                 <Content className="site-main">
-                    <Route path="/icon" component={IconExample}/>
+                    <Route path="/icon" component={IconDemo}/>
                     <Route path="/button" component={ButtonExample}/>
                     <Route path="/dialog" component={DialogExample}/>
                     <Route path="/layout" component={LayoutExample}/>
