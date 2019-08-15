907daa7a4a0e06ea17333c2512b8c87e665b254e
zhangyongwang <20225614@qq.com>
2019-08-15 16:11:01 +0800

    继续写 表单验证

--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -13,13 +13,28 @@ interface FormErrors {
     [K: string]: string[]
 }
 
+function isEmpty(value: any) {
+    return value === undefined || value === null || value === '';
+}
+
 const Validator = (formValue: FormValue, rules: FormRules): FormErrors => {
     let errors: any = {};
     rules.map(rule => {
         const value = formValue[rule.key];
         if (rule.required) {
             if (value === undefined || value === null || value === '') {
-                errors[rule.key] = ['必填'];
+                if (errors[rule.key] === undefined) {
+                    errors[rule.key] = []
+                }
+                errors[rule.key].push('必填');
+            }
+        }
+        if (rule.minLength) {
+            if (!isEmpty(value) || value!.length < rule.minLength) {
+                if (errors[rule.key] === undefined) {
+                    errors[rule.key] = []
+                }
+                errors[rule.key].push('太短');
             }
         }
         console.log(rule);
