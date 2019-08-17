9f18746feb2567c3a5d0300bbf9d490b8c4c45d5
zhangyongwang <20225614@qq.com>
2019-08-17 12:52:45 +0800

    给 Input 组件 写点样式

--- lib/input/input.tsx
+++ lib/input/input.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import {InputHTMLAttributes} from "react";
+import classes from "../helpers/classes";
+import './input.scss';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
 
 
 }
 
-const Input: React.FunctionComponent<Props> = () => {
+const Input: React.FunctionComponent<Props> = (props) => {
+    const {className, ...restProps} = props;
     return (
-        <input type="text"/>
+        <input className={classes('react-ui-input', className)}
+               {...restProps}/>
     )
 };
 
