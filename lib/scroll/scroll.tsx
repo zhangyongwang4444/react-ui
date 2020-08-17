cc2484e10640d034a6eda309215144df9e91ffef
zyw <20225614@qq.com>
2020-08-17 23:06:39 +0800

    create scrollbar

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React, {HTMLAttributes} from 'react';
+import React, {HTMLAttributes, UIEventHandler, useEffect, useRef, useState} from 'react';
 import './scroll.scss';
 import scrollbarWidth from './scrollbar-width';
 
@@ -8,10 +8,26 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
     const {children, ...rest} = props;
+    const [barHeight, setBarHeight] = useState(0);
+    const onScroll: UIEventHandler = (e) => {
+
+    };
+    const containerRef = useRef<HTMLDivElement>(null);
+    useEffect(() => { //mounted
+        const scrollHeight = containerRef.current!.scrollHeight;
+        const viewHeight = containerRef.current!.getBoundingClientRect().height;
+        setBarHeight(viewHeight * viewHeight / scrollHeight);
+
+    }, []);
     return (
         <div className='react-ui-scroll' {...rest}>
-            <div className='react-ui-scroll-inner' style={{right: -scrollbarWidth()}}>
+            <div className='react-ui-scroll-inner' style={{right: -scrollbarWidth()}}
+                 ref={containerRef}
+                 onScroll={onScroll}>
                 {children}
+            </div>
+            <div className="react-ui-scroll-track">
+                <div className="react-ui-scroll-bar" style={{height: barHeight}}/>
             </div>
         </div>
     );
